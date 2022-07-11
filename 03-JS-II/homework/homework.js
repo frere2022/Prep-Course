9fce71e5cdc7a8fc17a8ba82ac3dcd9ceb099415
Alexander Orozco <alex200663@gmail.com>
2022-07-11 15:51:00 -0500

    arreglado fizzBuzz  del  homework 03-JS-II

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -123,10 +123,10 @@ function fizzBuzz(numero) {
   if (numero % 3 === 0){
     return "fizz";    
   }
-  else if (numero % 5 === 0){
+  if (numero % 5 === 0){
     return "buzz";
   }
-  else if (numero % 15 === 0){
+  if (numero % 15 === 0){
     return "fizzbuzz";
   }
   else {
