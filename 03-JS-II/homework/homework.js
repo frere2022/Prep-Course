9385b93c8778ec7b174a5530c64fffbc5108ad05
Alexander Orozco <alex200663@gmail.com>
2022-07-12 18:52:52 -0500

    hecho operadoresLogicos del  homework 03-JS-II

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -144,7 +144,7 @@ function operadoresLogicos(num1, num2, num3) {
   if (((num1 > num2) && (num1 > num3)) && (num1 > 0)){
     return "Número 1 es mayor y positivo";
   }
-  else if (((num1 < 0) || (num2 < 0)) || (num3 < 0)){
+  else if ((num1 < 0 || num2 < 0) || num3 < 0){
     return "Hay negativos";
   }
   else if (num3 > num1 && num3 > num2){
