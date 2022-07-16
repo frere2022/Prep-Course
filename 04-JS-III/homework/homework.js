aff5c26947fbb873a073efc91823e15581d2e240
Alexander Orozco <alex200663@gmail.com>
2022-07-15 21:16:21 -0500

    voy en arrayContiene de  04-JS-III

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -76,6 +76,16 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+  var esta;
+  for (var i=0; i < array.length; i++){
+    if (array[i] === elemento){
+      esta = true;
+    }
+    else {
+      esta = false;
+    }
+  }
+  return esta;
 }
 
 
