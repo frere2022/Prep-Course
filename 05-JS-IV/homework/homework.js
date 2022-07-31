e4a86d56d54ec25d8bec8a69c63c506d326ce6ab
Alexander Orozco <alex200663@gmail.com>
2022-07-31 18:12:26 -0500

    Comencé el ejercicio 05-JS-IV

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,6 +6,14 @@ function crearGato (nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+  var Gato = {
+           nombre : nombre,
+           edad : edad,
+           meow : function (){
+            return "Meow!";
+           }    
+  };
+  return Gato;
 }
 
 function agregarPropiedad (objeto, property) {
