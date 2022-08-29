56d184ee7d2e3e960b944a5b4a667309af67c800
Alexander Orozco <alex200663@gmail.com>
2022-08-29 18:56:10 -0500

    vamos en multiplicarNumeroDesconocidoPorCinco de  05-JS-IV

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -37,7 +37,7 @@ function multiplicarNumeroDesconocidoPorCinco (objetoMisterioso) {
   // "objetoMisterioso" tiene una propiedad llamada "numeroMisterioso"
   // Multiplica el numeroMisterioso por 5 y devuelve el producto
   // Tu código:
-  var numeroDesconocido = objetoMisterioso[numeroMisterioso] * 5;
+  var numeroDesconocido = objetoMisterioso.numeroMisterioso * 5;
   return numeroDesconocido;
 }
 
