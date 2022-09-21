9c25ad29583952412fe0dedb066fb95e2fae8ff9
Alexander Orozco <alex200663@gmail.com>
2022-09-21 14:44:46 -0500

    vamos en tieneEmail  de  05-JS-IV

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -68,6 +68,11 @@ function tieneEmail (usuario) {
   // Devuelve "true" si el usuario tiene un valor definido para la propiedad "email"
   // De lo contratio, devuelve "false"
   // Tu código:
+  if(usuario['email']) {  
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function tienePropiedad (objeto, propiedad) {
